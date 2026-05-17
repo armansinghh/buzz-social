d9b201ba6e359456edc19a5ea90751c2a01dc001
ArmanSingh24 <armansingh6692@gmail.com>
2026-05-17 10:17:12 +0530

    chore(config): setup environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrV33zfMBme_gWggSkPOBTzSyVzQUcaDU",
-  authDomain: "buzz-social-52827.firebaseapp.com",
-  projectId: "buzz-social-52827",
-  storageBucket: "buzz-social-52827.firebasestorage.app",
-  messagingSenderId: "974839938234",
-  appId: "1:974839938234:web:7c39de25977f91b1875683"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
